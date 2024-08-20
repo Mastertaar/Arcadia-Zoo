22ace774fc9c87421405f760af240cc8ef6a483a
Romain Pitarque <mastertaar_97@hotmail.com>
2024-08-20 20:05:27 +0200

    responsive scrolling_menu v1 ended

--- public/js/login.js
+++ public/js/login.js
@@ -22,3 +22,17 @@ form.addEventListener("submit", () => {
         }
       })
 })
+
+/*Burger-Menu SCRIPT*/
+
+
+  const menuBtn = document.querySelector('.menu-btn')
+  const menuBtnIcon = document.querySelector('.menu-btn i')
+  const burgerMenu = document.querySelector('.burger-menu')
+
+  menuBtn.onclick = function() {
+    burgerMenu.classList.toggle('open')
+    const isOpen = burgerMenu.classList.contains('open') 
+    menuBtnIcon.classList = isOpen ? 'fa-solid fa-xmark' : 'fas fa-bars'
+  }
+
